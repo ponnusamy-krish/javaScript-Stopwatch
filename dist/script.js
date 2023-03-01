80133730a5c1fdb3285f7579e6b937a77fd2b6fe
sam <ponnusamykrish23@gmail.com>
2023-03-01 21:01:50 +0530

    "added js"

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,29 @@
-let hr = document.querySelector("#hr").textContent
-let min = document.querySelector("#min").textContent
-let sec = document.querySelector("#min").textContent
+var hr = document.querySelector("#hr")
+let min = document.querySelector("#min")
+let sec = document.querySelector("#sec")
 
+let startBtn = document.querySelector("#startBtn")
+let stopBtn = document.getElementById("stopBtn")
+let resetBtn = document.querySelector("#resetBtn")
+
+let secTimer,minTimer,hrTimer ;
+
+startBtn.addEventListener("click", ()=>{
+ secTimer = setInterval(secFunc,1000);
+//  minTimer = setInterval()
+})
+
+function secFunc(){
+        
+        sec.textContent++
+    
+}
+
+// stopBtn.addEventListener("click", ()=>{
+//     clearInterval(setInterval)
+//     console.log("hello");
+// })
+
+stopBtn.addEventListener("click", ()=>{
+    clearInterval(secTimer)
+})
