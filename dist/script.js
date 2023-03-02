612a8167e4c1c10eb84934248814c1c4f37511b0
sam <ponnusamykrish23@gmail.com>
2023-03-02 06:45:35 +0530

    correct seconds

--- dist/script.js
+++ dist/script.js
@@ -1,61 +1,51 @@
-var hr = document.querySelector("#hr")
-let min = document.querySelector("#min")
-let sec = document.querySelector("#sec")
+var hr = document.querySelector("#hr");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
 
-let startBtn = document.querySelector("#startBtn")
-let stopBtn = document.getElementById("stopBtn")
-let resetBtn = document.getElementById("resetBtn")
+let startBtn = document.querySelector("#startBtn");
+let stopBtn = document.getElementById("stopBtn");
+let resetBtn = document.getElementById("resetBtn");
 
-let secTimer,minTimer,hrTimer ;
+let secTimer, minTimer, hrTimer;
 
-startBtn.addEventListener("click", ()=>{
- secTimer = setInterval(secFunc,1000);
- minTimer = setInterval(minFunc,60000);
- hrTimer = setInterval(hrFunc,3600000)
-})
+startBtn.addEventListener("click", () => {
+  secTimer = setInterval(secFunc, 1000);
+  minTimer = setInterval(minFunc, 60000);
+  hrTimer = setInterval(hrFunc, 3600000);
+});
 
+resetBtn.addEventListener("click", () => {
+  hr.textContent = "00";
+  min.textContent = "00";
+  sec.textContent = "00";
+  clearInterval(secTimer);
+  clearInterval(minTimer);
+  clearInterval(hrTimer);
+});
 
-resetBtn.addEventListener("click", ()=>{
-    hr.textContent = "00"
-    min.textContent = "00"
-    sec.textContent = "00"
-    clearInterval(secTimer)
-    clearInterval(minTimer)
-    clearInterval(hrTimer)
-})
-
-
-
-
-
-
-function secFunc(){
-    
-    if(sec.textContent >=59){
-        sec.textContent = "00"
-    }
-    sec.textContent++
-    
+function secFunc() {
+  if (sec.textContent >= 60) {
+    sec.textContent = "00";
+  }
+  sec.textContent++;
 }
 
-function minFunc(){
-    if(min.textContent >=59){
-        min.textContent = "00"
-    }
-    min.textContent++
+function minFunc() {
+  if (min.textContent >= 60) {
+    min.textContent = "00";
+  }
+  min.textContent++;
 }
 
-function hrFunc(){
-    if(hr.textContent >= 2){
-        hr.textContent =="00"
-    }
-hr.textContent++
-
+function hrFunc() {
+  if (hr.textContent >= 2) {
+    hr.textContent == "00";
+  }
+  hr.textContent++;
 }
 
-
-stopBtn.addEventListener("click", ()=>{
-    clearInterval(secTimer)
-    clearInterval(hrTimer)
-    clearInterval(minTimer)
-})
+stopBtn.addEventListener("click", () => {
+  clearInterval(secTimer);
+  clearInterval(hrTimer);
+  clearInterval(minTimer);
+});
