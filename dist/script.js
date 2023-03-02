285eb56e3d644b1cc2788dcabffc916e3f49e6b9
sam <ponnusamykrish23@gmail.com>
2023-03-02 06:33:16 +0530

    finished

--- dist/script.js
+++ dist/script.js
@@ -4,26 +4,58 @@ let sec = document.querySelector("#sec")
 
 let startBtn = document.querySelector("#startBtn")
 let stopBtn = document.getElementById("stopBtn")
-let resetBtn = document.querySelector("#resetBtn")
+let resetBtn = document.getElementById("resetBtn")
 
 let secTimer,minTimer,hrTimer ;
 
 startBtn.addEventListener("click", ()=>{
  secTimer = setInterval(secFunc,1000);
-//  minTimer = setInterval()
+ minTimer = setInterval(minFunc,60000);
+ hrTimer = setInterval(hrFunc,3600000)
 })
 
+
+resetBtn.addEventListener("click", ()=>{
+    hr.textContent = "00"
+    min.textContent = "00"
+    sec.textContent = "00"
+    clearInterval(secTimer)
+    clearInterval(minTimer)
+    clearInterval(hrTimer)
+})
+
+
+
+
+
+
 function secFunc(){
-        
-        sec.textContent++
+    
+    if(sec.textContent >=59){
+        sec.textContent = "00"
+    }
+    sec.textContent++
     
 }
 
-// stopBtn.addEventListener("click", ()=>{
-//     clearInterval(setInterval)
-//     console.log("hello");
-// })
+function minFunc(){
+    if(min.textContent >=59){
+        min.textContent = "00"
+    }
+    min.textContent++
+}
+
+function hrFunc(){
+    if(hr.textContent >= 2){
+        hr.textContent =="00"
+    }
+hr.textContent++
+
+}
+
 
 stopBtn.addEventListener("click", ()=>{
     clearInterval(secTimer)
+    clearInterval(hrTimer)
+    clearInterval(minTimer)
 })
